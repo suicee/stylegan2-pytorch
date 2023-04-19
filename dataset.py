4b5035579d376bf1304d6c9f4e0e036e8ac54358
Ce Sui <157346111@qq.com>
2023-04-19 23:13:47 +0800

    add naive dataloader

--- dataset.py
+++ dataset.py
@@ -1,9 +1,15 @@
 from io import BytesIO
 
-import lmdb
+# import lmdb
 from PIL import Image
 from torch.utils.data import Dataset
-
+from natsort import natsorted
+import logging
+import torch
+import re
+import os
+import numpy as np
+from glob import glob
 
 class MultiResolutionDataset(Dataset):
     def __init__(self, path, transform, resolution=256):
@@ -38,3 +44,37 @@ class MultiResolutionDataset(Dataset):
         img = self.transform(img)
 
         return img
+
+import h5py
+
+class NaiveDataset(Dataset):
+    def __init__(self, data_dir,norm=False):
+      
+        with h5py.File(data_dir, 'r') as F:
+            images = np.array(F['images'])
+            labels = np.array(F['ans'])
+
+        total_data=np.transpose(images[:10000],axes=(0,3,1,2))
+            
+        logging.info(f'Creating dataset with {total_data.shape[0]} examples')
+
+        if norm:
+            self.total_data=self.preprocess(total_data,tp='data',mean=self.data_mean,std=self.data_std)
+        else:
+            self.total_data=total_data
+
+    def __len__(self):
+        return (self.total_data.shape[0])
+
+
+    @classmethod
+    def preprocess(cls,data):
+
+        return data
+
+    def __getitem__(self, i):
+        data=self.total_data[i]
+
+        return {
+            'data': torch.from_numpy(data).type(torch.FloatTensor),
+        }
